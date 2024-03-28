edb019639d645be0cf809427370b95d134024a29
Serega <26937163+serega-fighter@users.noreply.github.com>
2024-03-28 16:13:49 +0300

    print stack

--- src/main.rs
+++ src/main.rs
@@ -21,13 +21,17 @@ unsafe fn gt_switch(new: *const ThreadContext) {
     );
 }
 
-
 fn main() {
     let mut ctx = ThreadContext::default();
     let mut stack = vec![0_u8; SSIZE as usize];
     unsafe {
         let stack_bottom = stack.as_mut_ptr().offset(SSIZE);
         let sb_aligned = (stack_bottom as usize & !15) as *mut u8;
+        for i in 0..SSIZE {
+            println!("stack mem: {}, val: {}",
+                     sb_aligned.offset(-i as isize) as usize,
+                     *sb_aligned.offset(-i as isize))
+        }
         std::ptr::write(sb_aligned.offset(-16) as *mut u64, hello as u64);
         ctx.rsp = sb_aligned.offset(-16) as u64;
         gt_switch(&mut ctx);
